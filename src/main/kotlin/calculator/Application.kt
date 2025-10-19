fd08f907ea95a83a7c0a01cab1265ee8cf90776b
hyewoon <likethebegin@naver.com>
2025-10-19 22:36:48 +0900

    feat: 사용자 입력기능 구현

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,12 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console.readLine
+
+
 fun main() {
     // TODO: 프로그램 구현
+    println("덧셈할 문자열을 입력해 주세요.")
+    val input = readLine()!!
+    println("결과 : $input")
+
 }

3cb8d3e72523721be8f15007d8183c4cbcd8d4b3
hyewoon <likethebegin@naver.com>
2025-10-20 00:07:15 +0900

    문자열 덧셈 계산기 구현완료(예외처리 추가)

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,20 +1,30 @@
 package calculator
 
 import camp.nextstep.edu.missionutils.Console.readLine
+import net.bytebuddy.pool.TypePool.Resolution.Illegal
 
 
 fun main() {
     // TODO: 프로그램 구현
     println("덧셈할 문자열을 입력해 주세요.")
-    val input = readLine()
-    val list = if (input.startsWith("//")) {
-        val code = input.substring(2, 3)
-        input.substring(5).trim().split(code)
-    } else if(input.isEmpty()){
-        0
-    } else{
+    val input = readLine()!!
+    if(input.isEmpty()) {
+        println("결과 : 0")
+        return
+    }
+
+    val list: List<String> = if (input.startsWith("//")) {
+        val divider: String = input.substring(2, 3)
+        input.substring(5).trim().split(divider)
+    } else {
         input.split(",", ":")
     }
 
-    println("결과 : $list")
+    val number: List<Int> = list.map{ it.toInt() }
+    number.forEach {
+        if (it <= 0) throw IllegalArgumentException()
+    }
+    val result = number.sum()
+
+    println("결과 : $result")
 }

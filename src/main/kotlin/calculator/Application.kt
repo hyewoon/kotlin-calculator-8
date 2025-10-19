2030467c464a38bb2f1b33f626a26615fdf3238f
hyewoon <likethebegin@naver.com>
2025-10-19 23:21:36 +0900

    refactor: 빈 문자열 입력 처리 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -6,7 +6,15 @@ import camp.nextstep.edu.missionutils.Console.readLine
 fun main() {
     // TODO: 프로그램 구현
     println("덧셈할 문자열을 입력해 주세요.")
-    val input = readLine()!!
-    println("결과 : $input")
+    val input = readLine()
+    val list = if (input.startsWith("//")) {
+        val code = input.substring(2, 3)
+        input.substring(5).trim().split(code)
+    } else if(input.isEmpty()){
+        0
+    } else{
+        input.split(",", ":")
+    }
 
+    println("결과 : $list")
 }
